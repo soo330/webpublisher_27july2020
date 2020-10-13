8472f421db999e0f0174829713b727f76bd4ab73
soo330 <SooPark93@gmail.com>
2020-10-13 18:03:06 +0900

    1013 updates

--- c_code/c_sample_web/js/1013webtest_01_result.js
+++ c_code/c_sample_web/js/1013webtest_01_result.js
@@ -42,3 +42,29 @@ var closeBtn = modalWin.children('.close');
 modalWin.on('click', function(){
   modalWin.fadeOut();
 });
+
+//=========================================================
+
+// 1013 노트필기
+
+//type 데이터 유형 -> 
+
+// 숫자 number 숫자는 ''처리 하지 않는다
+// ex) a=10 b='10'
+//     a+10= 20 b+10=1010
+
+// 문자 string / 문자는 반드시 값이 있을때 '값' 처리해서 처리한다
+// 참/거짓 true/false (boolean)
+// 초기화/ 미정의 null(기존에 있던 것을 초기화 한 것=reset)/undefined
+// 배열 객체 함수
+
+
+//javascript에서는 구분을 위해 ;이 아니라 ,쉼표로 구분한다 반드시
+
+// -> 배열 목록 array []
+//    객체 정의 object {}
+//    함수 기능수행 function (){}
+
+// ex> main > ul> li> a 를 쓸때
+//    var
+
