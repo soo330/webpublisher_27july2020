877a6599240e971ab00864844064ef6ecda96151
soo330 <SooPark93@gmail.com>
2020-10-28 14:57:02 +0900

    1028 object

---
+++ c_code/d_javascript/js/object.js
@@ -0,0 +1,101 @@
+// object.js
+
+// var obj = new Object;
+/*
+var obj = {
+  'samsung':'galaxy',
+  'apple' : 'iphone',
+  'google' : 'pixel',
+  'lg' : 'belbet'
+};
+*/
+// console.log(obj);
+/*
+obj.nokia = 'banana';
+// console.log(obj)
+obj.Fn= function(person){
+  return person + ' 님 반가워요!';
+};
+
+var x = obj.Fn('xido');
+// console.log(x);
+*/
+/*
+var arrObj = [
+  {'title' : 'apple',
+  'serise' : 'iphone',
+  'site' : 'http://apple.com',
+  'content' : '2020년 애플의 최신 아이폰 출시'
+},
+{'company' : 'samsung',
+  'serise' : 'galaxy',
+  'site' : 'http://samsung.com',
+  'content' : '갤럭시21 부품유출'
+},
+{ 'company': 'lg',
+  'serise' : 'wing',
+  'site' : 'https://lg.com',
+  'content' : '과거의 가로본능을 기억하게하는 윙'
+
+},
+{ 'company' : 'xiaomi',
+  'serise' : 'oneplus',
+  'site' : 'https://onplus.com',
+  'context' : '삼성, 애플에 뒤쳐지지 않는 성능 하지만 중국거라는 슬픈현실'
+}
+
+];
+*/
+// console.log(arrObj.length);
+// console.log(arrObj[0].serise);
+/*
+var p=0;
+for(; p < arrObj.length ; p += 1){
+  console.log((p+1) + '. ' + arrObj[p].serise);
+}
+*/
+// (p+1)은 앞의 목록을 지정가능
+// forEach();
+// arrObj.forEach(function(data){
+//   console.log(data.serise)
+// });
+
+// arrObj.forEach(function(data,idx){
+//   console.log( (idx + 1 ) + '. ' + data.serise)
+// });
+// ===============================================
+/*
+var list = ['apple','iphone','lg','oneplus'];
+var link = [
+  'http://apple.com', 'http://samsung.com', 'https://lg.com', 'https://onplus.com'];
+var content = ['2020년 애플의 최신 아이폰 출시', '갤럭시21 부품유출', '과거의 가로본능을 기억하게하는 윙']
+var img.style
+
+var product = document.getElementsByClassName('product')[0];
+var z = 0;
+var listLen = list.length;
+
+for( z=0; z<listLen ; z+=1 ){
+    div = document.createElement('div');
+    h3= document.createElement('h3');
+    p = document.createElement('p');
+    p2 = document.createElement('p');
+    div.classList.add(list[z]);
+    h3.innerText = list[z];
+    p.innerText = list[z];
+    p2.innerText = content[z];
+    p.setAttribute('href', link[z]);
+    product.append(div);
+    div.append(h3);
+    div.append(p);
+    div.append(p2);
+  };
+  // console.log(arrObj[0].site);
+
+  */
+
+  // =======================================================
+
+  // 풀이
+
+  
