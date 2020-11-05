20706c77927a8254d65813fd197c0c1310280790
soo330 <SooPark93@gmail.com>
2020-11-05 16:15:39 +0900

    1105 miniproduct2 update

---
+++ c_code/c_sample_web/js/src/miniproduct2.js
@@ -0,0 +1,13 @@
+(function($){
+  var miniP = $('#miniProduct');
+  var proWrap = miniP.find('.product_wrap');
+  var backImg = proWrap.find('.product2');
+  var backL = backImg.find('li');
+
+  var slideIndi = miniP('.slide_indi'); //div
+  var slideIndi2Ul = slideIndi.find('.indicator2');
+  
+  var backLLast = backL.eq(-1).clone();
+  backImg.prepend(backLLst);
+  var 
+})(jQuery);
