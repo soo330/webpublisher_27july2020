77d423abd34f78493928a8b741c41adb2353e8ab
soo330 <SooPark93@gmail.com>
2020-11-05 15:33:45 +0900

    1105 monffee viewBox update

--- c_code/c_sample_web/js/src/monffee_viewBox.js
+++ c_code/c_sample_web/js/src/monffee_viewBox.js
@@ -1,9 +1,42 @@
-
 // monffee_viewBox.js
+
+  
+// var count = 0;
+// setTimeOut(function(){},시간); 0.1초 뒤에
+// setInterval(function(){},시간);
+// clearInterval(setInterval함수이름);
+
+// var Go =setInterval(function(){
+//   count += 1;
+//   console.log(count);
+//   if( count > 20 ){
+//     clearInterval(Go); // break문과 비슷함 setInterval을 날려라
+//   }
+// },1000);
+
 (function($){
+//   var count = 0;
+//   var Start; // undefind
+//   var MyCount = function(){
+//     Start = setInterval(function(){
+//     count += 1;
+//     // console.log(count);
+//   },500);
+// };
+//   MyCount();
+
+//   $('h1').on('mouseenter', function(){
+//     clearInterval( Start ); //위의 start라는 변수를 지워라
+//   });
+//   $('h1').on('mouseenter', function(){
+//     count= 0; // 마우스를 올렸다가 떼면 다시 카운트해라
+//     MyCount();
+//   });
+
+
   var viewBox = $('#viewBox');
 
-  var backImg = viewBox.find('.back_img');
+  var backImg = viewBox.find('.back_img'); //ul
   viewBox.css({'overflow':'hidden'}); // 가급적 css에서 처리하자
   var backLi = backImg.find('li');
   var permission = true; // 워터슬라이드같은 개념 하나보내고 하나보냄
@@ -25,7 +58,11 @@
   var prevSlideBtn = slideBtn.children('button').eq(1); // prev 버튼
   // console.log(nextSlideBtn, prevSlideBtn);
 
-  var slideN = 0;
+  var slideN = 0; //최초의 값
+  var timed =2000; // 일정시간 마다 처리하게하는 시간
+
+
+  // 다음버튼 클릭========================================
   nextSlideBtn.on('click', function(e){
     // a 또는 button 요소처럼 이벤트 기능이 이미 내장된 요소는 미리 해당 기능을 제거하라는 옵션을 먼저 넣는다
     e.preventDefault();
@@ -51,7 +88,7 @@
       }
       // permission=true;
       // setTimeOut(function(){},시간); 뒤에있는 0.1초 시간 뒤에 함수를 시작하세요
-      setTimeOut(function(){
+      setTimeout(function(){
         permission=true; //여기에 permission을 주면 
       },100);
     });
@@ -59,21 +96,21 @@
     indiLi.eq(slideN).addClass('action');}
   });
 
-  
+  // 이전버튼 클릭========================================
   prevSlideBtn.on('click', function(e){
     e.preventDefault();
     if(permission){
       permission =false;
       slideN -=1;
       backImg.stop().animate({'left': slideN * -100 + '%'},function(){
-      if(slideN <= -1 /*여기서의 -1이 순번째라고 이해하면 쉽다*/ ){
+        if(slideN <= -1 /*여기서의 -1이 순번째라고 이해하면 쉽다*/ ){
         slideN = backLi.length-1;
         backImg.stop().css({'left': slideN * -100 + '%'});
-      };
+    };
       setTimeOut(function(){permission=true;},1000);
     });
     indiLi.eq(slideN).siblings().removeClass('action');
-    indiLi.eq(slideN).addClass('action'); //인디케이터도 같이넘어감}
+    indiLi.eq(slideN).addClass('action'); //인디케이터도 같이넘어감
     };
   });
   // -----------------------------------------
@@ -84,7 +121,7 @@
   // ↓↓↓↓ 선택한 li들중에서 라는 예문
   indiLi.on('click', function(e){
     e.preventDefault();
-    var its = $(this); ////$(this)는 click했을때 선택되는 것'들' 중복되는값
+    var its = $(this); //$(this)는 click했을때 선택되는 것'들' 중복되는값
     slideN = its.index(); //.index는 순서를 확인하는 아이
     // console.log(itI);
     backImg.stop().animate({'left': slideN * -100 + '%'});
@@ -103,8 +140,8 @@
   //   });
   //});
 
-  // ============================================================
-  // indicator 클릭시
+  // 인디케이터 포커스 버튼 클릭======================================================
+  //
   indiLi.children('a').on('focus', function(e){
     e.preventDefault();
     var its= $(this); 
@@ -114,6 +151,34 @@
     indiLi.eq(slideN).addClass('action');
     //** 계속 반복문이 있으므로 함수도 생각해봐야함
   });
+
+  // 광고영역 일정시간마다 자동수행/ 마우스 올릴경우 일시정지 =======================
+
+  var startInterval;
+  var Start = function(){
+    // setInterval 일정시간마다 ~실행해라
+    startInterval = setInterval(function(){
+      // trigger() 기능 - 대신 처리하는 방아쇠
+      nextSlideBtn.trigger('click');
+    }, timed);
+  };
+
+  // 1. viewBox 마우스올리면 일시정지해라
+  var StopSlide = function(){
+    clearInterval(startInterval);
+  };
+  viewBox.on('mouseenter', function(){
+    clearInterval(startInterval);
+    StopSlide();
+  });
+
+  //2. viewBox 에서 마우스 벗어나면 재실행
+
+  viewBox.on('mouseleave', function(){
+    Start();
+  });
+
+  //jQuery End
 })(jQuery);
 
 /*
@@ -130,9 +195,9 @@ dt~dd -> $('dt~dd'); -> 권장 $('dt').nextAll('dd');
 이전 형제 모두       -> 권장 $('dt').prevAll('dd');
 
 자신을 제외한 형제   -> $('dt').siblings('dd');
-부모+형제를 선택 -> $('dt').parent('dl'); $('dt').parentUntil('dd');
------------------------------------------------------------------
-** css
+부모+형제를 선택 -> $('dt').parent('dl'); $('dt').parentUntil('dd');*/
+// -----------------------------------------------------------------
+/* css
 선택자.style.backgroundColor = '#acf';
 
 선택자.css({속성명:'속성갑',(코마) 속성명:'속성갑',(코마) });
@@ -145,17 +210,12 @@ var reFn = function(i){
   }
   else{console.log('20이 되었습니다');}
 };
-
-
- 
-jQuery에서 콜백함수란?
-하나의 method(함수) 를 수행한 후에, 다시 함수를 수행할 수 있도록 처리하는 형태
-단, anmimation 기능을 가진 method 내 및 이벤트에서만 콜백 함수의 기능을 가질 수 있다
-
-선택자.on('click'1), function(){
-  선택자.anmate({기능수행}, 2)function(){3)})
-차례는 1) -> 2) -> 3) 의 순서로 진행됨
-})
-
 */
 
+// jQuery에서 콜백함수란?
+// 하나의 method(함수) 를 수행한 후에, 다시 함수를 수행할 수 있도록 처리하는 형태
+// 단, anmimation 기능을 가진 method 내 및 이벤트에서만 콜백 함수의 기능을 가질 수 있다
+
+// 선택자.on('click'1), function(){
+//   선택자.anmate({기능수행}, 2)function(){3)})
+// 차례는 1) -> 2) -> 3) 의 순서
