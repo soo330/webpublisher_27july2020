17a7555f25e3a982961a6c12a4e69f3a28f71821
soo330 <SooPark93@gmail.com>
2020-11-06 17:38:10 +0900

    1106 jq_03-1_scroll.js

---
+++ c_code/e_jquery/js/src/jq_03-1_scroll.js
@@ -0,0 +1,31 @@
+// jq_03-1_scroll.js
+
+(function($){
+  var win = $(window); //따옴표 안씀
+  var wrap = $('#wrap');
+  // wrap.height(3000);
+  wrap.css({'height': 3000+'px','backgroundImage':'linear-gradient(45deg, #ccc, #fca)'});
+
+  /** offset
+  선택자.offset().top, 선택자.offset().left
+  -> 브라우저 기준 0,0 좌표에서 부터
+  선택자가 얼만큼 떨어져있는지를 판단하는 기능
+  */
+
+  var fix = $('.fix');
+  var offsetTop = fix.offset().top; //선택자가 얼만큼 떨어져있는가?
+  // console.log( offsetTop );
+  win.on('scroll',function(){
+    var st = win.scrollTop(); // 스크롤이 얼만큼 이동했는가
+    // console.log(st);
+
+    if( offsetTop < st ){
+      fix.css({position:'fixed'});
+    }else{
+      // fix.css({position: 'relative'});
+      fix.removeAttr('style');
+    }
+  })
+
+
+})(jQuery);
