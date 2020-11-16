0bb4f5ab69680cf25f59410c1f84cd12c6dd0d6e
soo330 <SooPark93@gmail.com>
2020-11-16 14:46:11 +0900

    1116 14:45 focus와 click처리의 if와 else if 문 작성하기

--- c_code/e_jquery/js/src/slideBtnBanner4.js
+++ c_code/e_jquery/js/src/slideBtnBanner4.js
@@ -8,20 +8,37 @@
     var slideWrap = IFS.find('.slide_wrap');
     var slideUl = slideWrap.children('ul');
     var slideLi = slideUl.children('li');
-    var SlideLiLink = slideLi.children('a');
+
+    var actionLi = slideUl.find('.action');
+    actionLi.show();
 
     var timed = 500;
+    var index=0;
 
     indiLi.children('a').on('click focus', function(e){
         e.preventDefault();
 
         var theLi = $(this);
         var theLiIndex = theLi.parent('li').index();
+        
         indiLi.eq(theLiIndex).addClass('action');
         indiLi.eq(theLiIndex).siblings().removeClass('action');
-        
+
+        if(e.type === 'focus'){
+            console.log('focus 처리 되었습니다');
+
+        }else if (e.type === 'click'){
+            console.log('click 처리 되었습니다');
+        }
         slideLi.eq(theLiIndex).addClass('action');
         slideLi.eq(theLiIndex).siblings().removeClass('action');
+        
+        // slideLi.eq(theLiIndex).fadeIn(function(){}); //fade In은 애니메이션이 생기니까
+        slideLi.eq(theLiIndex).fadeIn(function(){
+            slideLi.eq(theLiIndex).siblings().hide();
+        }); //slide fadeIn
+        // ==============================================================
+
     }); //indiLi on
     
 })(jQuery);
