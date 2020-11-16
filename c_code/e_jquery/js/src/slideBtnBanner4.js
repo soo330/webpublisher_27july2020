3522d53c19edbae9b6150ec6c077278043387a33
soo330 <SooPark93@gmail.com>
2020-11-16 15:22:15 +0900

    1116 15:21 focus 할때 마지막에서 사진 안넘어가는 것 에러 수정

--- c_code/e_jquery/js/src/slideBtnBanner4.js
+++ c_code/e_jquery/js/src/slideBtnBanner4.js
@@ -20,25 +20,37 @@
 
         var theLi = $(this);
         var theLiIndex = theLi.parent('li').index();
-        
-        indiLi.eq(theLiIndex).addClass('action');
-        indiLi.eq(theLiIndex).siblings().removeClass('action');
+        actionLi = slideUl.find('.action');
 
-        if(e.type === 'focus'){
-            console.log('focus 처리 되었습니다');
+        // indiLi.eq(theLiIndex).addClass('action');
+        // indiLi.eq(theLiIndex).siblings().removeClass('action');
 
-        }else if (e.type === 'click'){
-            console.log('click 처리 되었습니다');
-        }
-        slideLi.eq(theLiIndex).addClass('action');
-        slideLi.eq(theLiIndex).siblings().removeClass('action');
+        // console.log(theLiIndex);
+
+        // slideLi.eq(theLiIndex).stop().addClass('action');
+        // slideLi.eq(theLiIndex).siblings().removeClass('action');
         
         // slideLi.eq(theLiIndex).fadeIn(function(){}); //fade In은 애니메이션이 생기니까
-        slideLi.eq(theLiIndex).fadeIn(function(){
-            slideLi.eq(theLiIndex).siblings().hide();
-        }); //slide fadeIn
+        // slideLi.eq(theLiIndex).stop().show();
+        // actionLi.eq(theLiIndex).siblings().stop().fadeOut(function(){
+        //     slideLi.eq(theLiIndex).siblings().removeClass('action');
+        //     slideLi.eq(theLiIndex).stop().addClass('action');
+
+        // }); //slide fadeOut
         // ==============================================================
 
+        if(theLiIndex !== actionLi.index()){
+            indiLi.eq(theLiIndex).addClass('action');
+            indiLi.eq(theLiIndex).siblings().removeClass('action');
+
+            slideLi.eq(theLiIndex).stop().addClass('action');
+            slideLi.eq(theLiIndex).siblings().removeClass('action');
+            actionLi = slideUl.find('.action');
+
+            slideLi.eq(theLiIndex).stop().fadeIn(function(){
+                slideLi.not(actionLi).stop().hide();
+            }); // slideLi fadeIn
+        } //if theLiIndex !==
     }); //indiLi on
     
 })(jQuery);
