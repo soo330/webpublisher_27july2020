56fb8602783c69a4a4c700496ab3858a2af3b18c
soo330 <SooPark93@gmail.com>
2020-11-17 14:23:18 +0900

    modal update

--- c_code/e_jquery/js/src/fancybox_plugin.js
+++ c_code/e_jquery/js/src/fancybox_plugin.js
@@ -47,9 +47,7 @@
   // modal.append(galleryCode); //선택자 내부의 뒤에
   modal.after(galleryCode); // 선택자 바로 뒤에
 
-
-
-  
+ 
   var galleryBox = $('.gallery_box');
   var galleryArea= galleryBox.children('.gallery_area');
   var close = galleryArea.find('.close');
@@ -78,15 +76,18 @@
       //여기서 modalLi를 쓸수 없는 이유는 변수 바깥에서 정의를 해줬기때문에
       //해당 변수는 이것을 이해하지 못하므로 
       
-      modalBtn.css({backgroundImage: 'url(' + url + data[i].thumImg+')', textTransform:'uppdercase'});
       temLi.find('span').text(data[i].text);
+      modalBtn.css({backgroundImage: 'url(' + url + data[i].thumImg+')', textTransform:'uppdercase'});
+      // 1)위값을 객체가 아닌 변수값으로 사용해보자
+      
 
       // temLi.attr({'data-big': data[i].bigImg ,'data-narr': data[i].content });
+      // 2)위값을 사용해서 객체가 아닌 변수값으로 사용해보자
     }
     var modalLi = modalUl.find('li');
     var mLibtn = modalLi.find('button');
     var span = mLibtn.find('span');
-    
+    var pTag = bigImg.children('p');
 		var indexCheck;
 
     modalLi.children('button').on('click', function(e){
@@ -95,19 +96,22 @@
 			var theLiI = theLi.index();
 
       indexCheck= theLiI;
+      
+      // thedataNarr = theLi.attr('data-narr');
+			// theBig = theLi.attr('data-big');      
+      // pTag.text(thedataNarr);
+      // bigImg.css({backgroundImage: 'url(' + url + theBig +')'});
 
-      dataNarr = theLi.attr('data-narr');
-      var pTag = bigImg.children('p');
-      pTag.text(dataNarr);
+      bigImg.css({backgroundImage: 'url(' + url + data[indexCheck].bigImg +')'});
+      pTag.text(data[indexCheck].content);
 
-			var theBig = theLi.attr('data-big');
 			galleryBox.stop().fadeIn(function(){
         close.children('button').focus();
         setTimeout(function(){
           pTag.addClass('action');
         },timed/2);
 			});
-      bigImg.css({backgroundImage : 'url('+ url + theBig +')'});
+      // bigImg.css({backgroundImage : 'url('+ url + theBig +')'});
     
     }); //mLiBtn button fadeIn
     
