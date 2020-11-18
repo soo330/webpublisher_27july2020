e048c54f618362b1cd3ceb62707fd5b98d441bd9
soo330 <SooPark93@gmail.com>
2020-11-18 12:11:35 +0900

    onpage scroll top으로 이동/ 상단메뉴에서 해당페이지 이동과 애니메이션

--- c_code/e_jquery/js/src/onepageScroll.js
+++ c_code/e_jquery/js/src/onepageScroll.js
@@ -5,9 +5,7 @@
   
   /*
   1. scroll시 $('#headBox') 상단고정
-
     1-1. 상단 버튼 기능 추가
-
   2. $('popup_dp') 닫기버튼 클릭 시 팝업 창 삭제
   */
 
@@ -15,15 +13,21 @@
   var win = $(window);
   var headBox = $('#headBox');
   // $(window).offset().top;
+  var navBox = $('#navBox');
+  var navUl = navBox.find('ul');
+  var navLi = navUl.find('li');
+  // var navA = navLi.find('a');
+  var topBtn = $('.top_move_btn');
+
 
   //*** browser 상단에서 떨어져있는 양 체크
   var headOffset = headBox.offset().top;
   // console.log('offset:', headOffset);
 
+  // ------------------------------------------------
   // *** 1-1. 보이지 않았다가 1000px 이동 후 나타나기
-  var topBtn = $('.top_move_btn');
-  topBtn.hide();
 
+  topBtn.hide();
 
   win.on('scroll', function(e){
     // win.scrollTop(); // 스크롤이 얼마나 움직였는지 이동한 위치 값 파악
@@ -31,15 +35,15 @@
     console.log(winSt);
 
     if( winSt >= headOffset ){
-      headBox.css({"position":'fixed', 'top':0, 'zIndex':1500});
+      headBox.css({"position":'fixed', 'top':0, 'zIndex':1500,});
     } // if
     else{
       headBox.removeAttr('style');
     } // else
-  }); //win.on(){}
 
 
-  // ==========================================
+
+  // ===================
 
   // if( winSt >= 500 ){
   //   topBtn.stop().fadeIn();
@@ -51,21 +55,51 @@
   // ------------------------
   // ↓↓↓↓↓ 이렇게 사용가능 ()괄호 제거하고 이해하기 
   // ( (조건) ) ? (참이면) : (거짓이면) ;
-    // ==========================================
+  // ====================
   ( winSt >= 500 ) ? topBtn.stop().fadeIn() : topBtn.stop().fadeOut();
+}); //win.on(){}
+
+
+
 
   topBtn.on('click', ['a'], function(e){
     e.preventDefault();
     var theA = $(this).find('a');
 
     // a요소의 연결된 선택자 파악
-    var theAAtter = theA.atter('href');
+    var theAAtter = theA.attr('href');
 
     // a요소의 href값의 상단에서 떨어져있는 양 체크
     var theAOffset = $(theAAtter).offset().top;
-    // 브라우저를 이동시켜라(스크롤) -> theAOffset으로 파악된 크기만큼
-    win.animate({scrollTop: theAOffset});
-  }) //on (){}
+    // 브라우저를 이동시켜라(스크롤) -> theAOffset 으로 파악된 크기만큼
+    console.log(theAOffset);
+    $('html,body').animate({scrollTop: theAOffset});
+
+  }) // topBtn.on (){}
+ //------------------------------------------
+  navLi.on('click', ['a'], function(e){
+    e.preventDefault();
+    var theNavLi = $(this).find('a');
+    var theNavLiAttr = theNavLi.attr('href');
+    var theNavOffset = $(theNavLiAttr).offset().top;
+    $('html,body').animate({ scrollTop: theNavOffset});
+  }); //navLi.on (){}
+
+  // ============================================
+  // ============================================
+  // 2. $('popup_dp') 닫기버튼 클릭 시 팝업 창 삭제
+  var popupDp = $('.popup_dp');
+  var popupBtn =popupDp.find('button');
+  popupBtn.on('click', function(e){
+    e.preventDefault();
+    popupDp.remove();
+  }); //popupBtn (click){}
+
+
+
+
+
+  
 
   //jQuery end
 })(jQuery);
