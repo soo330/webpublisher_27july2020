99e7481a4e0c3a101eff4918b2b546a43840fcb8
soo330 <SooPark93@gmail.com>
2020-11-19 17:09:55 +0900

    1119 update

--- c_code/e_jquery/js/src/rwdTemp_import_module.js
+++ c_code/e_jquery/js/src/rwdTemp_import_module.js
@@ -1,4 +1,5 @@
 // rwdTemp_import_module.js
+// rwdTemp_import_module.js
 
 (function($){
   var nowW = deviceSize();
@@ -7,19 +8,33 @@
   var deviceType = ['mobile', 'tablet', 'laptop','pc','pcfull'];
   // -----------------------------------------------
   var url = './rwdTemp/';
+  var headBox= $('#headBox');
   var viewBox= $('#viewBox');
+  // -----------------------------------------------
+  //viewBox import file
+  var viewImport = function(){
+    viewBox.append('<script src="../js/src/temp_mobile.js></script>');
+  }
   // -----------------------------------------------
 
 
   if(nowW === deviceType[0]){
-    // viewBox.load(url+ );
+    headBox.load( url + 'rwdTemp_headBox_mobile.html', function(){
+
+    })
+    viewBox.load(url+ 'rwdTemp_mobile.html', function(){
+      $(this).append('<script src="../js/src/temp_mobile.js></script>');
+    });
   } //if
   else if( nowW === deviceType[1]){
-
+    headBox.load( url + 'rwdTemp_headBox_mobile.html')
+    viewBox.load(url+ 'rwdTemp_mobile.html', viewImport );
   } //else if
   else{
-
+    headBox.load( url + 'rwdTemp_headBox_mobile.html')
+    viewBox.load(url+ 'rwdTemp_mobile.html', viewImport );
   } //else
+
 
   //jQuery end
 })(jQuery);
