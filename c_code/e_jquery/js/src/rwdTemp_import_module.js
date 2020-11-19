85dbd948edfdd423a16d6e497654f98fa0ea86a4
soo330 <SooPark93@gmail.com>
2020-11-19 16:33:10 +0900

    만들어놓은 디바이스 사이즈 불러오기

---
+++ c_code/e_jquery/js/src/rwdTemp_import_module.js
@@ -0,0 +1,25 @@
+// rwdTemp_import_module.js
+
+(function($){
+  var nowW = deviceSize();
+  // console.log(nowW);
+
+  var deviceType = ['mobile', 'tablet', 'laptop','pc','pcfull'];
+  // -----------------------------------------------
+  var url = './rwdTemp/';
+  var viewBox= $('#viewBox');
+  // -----------------------------------------------
+
+
+  if(nowW === deviceType[0]){
+    // viewBox.load(url+ );
+  } //if
+  else if( nowW === deviceType[1]){
+
+  } //else if
+  else{
+
+  } //else
+
+  //jQuery end
+})(jQuery);
