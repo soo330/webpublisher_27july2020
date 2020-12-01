a479b54116979e7fd54bb444512a9aa5eec2ad2e
soo330 <SooPark93@gmail.com>
2020-12-01 16:35:45 +0900

    16:35 sort function(a,b){}로 배열방식 만들기

--- c_code/e_jquery/js/src/notice.js
+++ c_code/e_jquery/js/src/notice.js
@@ -8,7 +8,10 @@
     context: document.body
 
   }).done(function(data){
-    var dataFile = data;
+    var dataFile = data.sort(function(a,b){
+      return b.id-a.id;
+    });
+
     console.log(dataFile);
     var noticeCode = '<li><a href="#"><em>5</em><span></span></a></li>';
     var indiCode = '<li class="action"><a href="#">01</a></li>';
@@ -17,23 +20,26 @@
     var notice = $('.notice_board');
     var noticeCon = notice.children('.context');
     var noticeArea = noticeCon.children('ul');
-    var indiCon = notice.children('.indicator')
+    var indiCon = notice.children('.indicator');
+    var indiLi = indiCon.children('li');
 
     // 내용 넣기
     var i = 0;
     var noticeLi;
+    
+
     for( ; i< dataFile.length; i+=1 ){
       // datafile의 길이만큼 가지고오고 i에 1씩 더해라
+
       noticeArea.append(noticeCode);
-      noticeLi = noticeArea.children('li').eq(i).; //eq i번째
+      noticeLi = noticeArea.children('li').eq(i); //eq i번째
 
-      noticeLi.find('em').text(dataFile[i].id);
+      noticeLi.find('em').text(dataFile[i].id );
       noticeLi.find('span').text(dataFile[i].address);
-
     }// for
 
     // 1. 순서 뒤집어서 배치
-    // 2. 한번에 보일내용을 적당히 줄여서 배치
+    // 2. 한번에 보일 내용을 적당히 줄여서 배치
 
 
   }); // ajax
