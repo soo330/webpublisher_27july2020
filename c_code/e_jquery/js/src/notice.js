a0d636d1b00f4fa0dbf89c74b693917d4a989064
soo330 <SooPark93@gmail.com>
2020-12-01 17:13:52 +0900

    1201 notice update

--- c_code/e_jquery/js/src/notice.js
+++ c_code/e_jquery/js/src/notice.js
@@ -10,14 +10,9 @@
   }).done(function(data){
     var dataFile = data.sort(function(a,b){
       return b.id-a.id;
-    });
+    }); //dataFile
 
-
-   
-  
-    
-
-  }); // done
+ 
 
     console.log(dataFile);
     var noticeCode = '<li><a href="#"><em>5</em><span></span></a></li>';
@@ -32,49 +27,55 @@
 
     // 내용 넣기
 
-    var reSetting = function(){}
-    var i = 0;
-    var noticeLi;
-    
+    var reSetting = function(){
+      var i = 0;
+      var noticeLi;
+      noticeArea.empty();      
+  
+      for( ; i< dataFile.length; i+=1 ){
+        // datafile의 길이만큼 가지고오고 i에 1씩 더해라
+        noticeArea.append(noticeCode);
+        noticeLi = noticeArea.children('li').eq(i); //eq i번째
+        noticeLi.find('em').text(dataFile[i].id );
+        noticeLi.find('span').text(dataFile[i].address);
+      }// for
+    } //reSetting
+    reSetting();
 
-    for( ; i< dataFile.length; i+=1 ){
-      // datafile의 길이만큼 가지고오고 i에 1씩 더해라
-      noticeArea.append(noticeCode);
-      noticeLi = noticeArea.children('li').eq(i); //eq i번째
-      noticeLi.find('em').text(dataFile[i].id );
-      noticeLi.find('span').text(dataFile[i].address);
-    }// for
 
     // 1. 순서 뒤집어서 배치
     // 2. 한번에 보일 내용을 적당히 줄여서 배치
     // 3. 오름차순, 내림 차순 연결해보기
 
-  }); // ajax
 
+    // ---------------------------------------
     // 아래 배열방식처럼 만든다
-   
+    // button을 만든다 
     var select_area = $('.select_area').find('button');
     select_area.on('click',function(e){
       e.preventDefault();
       var i = $(this).index();
-      switch(k){
+      switch(i){
       case 0:
         dataFile = data.sort(function(a,b){
           return b.id-a.id;
 
         }); //data.sort
-        break //
+        reSetting();
+        break
       // ------------------------
+    
       case 1:
       dataFile= data.sort(function(a,b){
         return a.id-b.id;
 
       }); //data.sort
-      break //
+      reSetting();
+      break
     } //switch
   
-  })// select_area
+  });// select_area
 
-  
-  //jQuery end
+}); // done
+//jQuery end
 })(jQuery);
