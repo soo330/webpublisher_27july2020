07847c72fa9b02b4ec72c10cdc668dbf42331d59
soo330 <SooPark93@gmail.com>
2020-12-04 16:22:15 +0900

    16:22 moreBtn 구성과 해석

--- c_code/e_jquery/js/src/notice.js
+++ c_code/e_jquery/js/src/notice.js
@@ -44,11 +44,12 @@
       indiLi.find('a').text( indiN + 1 );
     } //for
     //-------------------------------------
+
     // 인디케이터 보일 갯수 조정하기
     var indiViewLen = 5; //보고싶은 갯수
     var iv = 0; //최초값
     indiLi = indiArea.children('li');
-    var memoryN; // 3) 변수를 준다
+    var memoryN; // 3) 변수를 준다 (임시기억장치)
 
     // indiLen: 인디갯수
     for( ; iv < indiLen ; iv += 1){
@@ -56,6 +57,9 @@
         // 1)iv값이 1개씩 올라갈때 보이고자 하는 갯수5이면
         // 
         memoryN = iv+1; // 4) 3번을 언급 기억할 n 는 iv와 같다
+        // memoryN은 보이는 인디li에서 마지막 순서값을 기억하게 하기위한 명칭
+
+
         continue; // 2) 5가 될때까지 계속 넘겨라(패스) 4번기능을 썼으므로 countinue는 지워줘도 된다
       } //if
       else {
@@ -63,19 +67,41 @@
       } //else
     } // for
 
+    /*
+    주석처럼 코드는 아래처럼 변경할 수있다
+    
+    for( ;  iv < 인디갯수 ; iv+= 1 ){
+      if(iv >= 5){
+        indiLi.eq(iv).hide();
+      }else{
+        memoryN = iv;
+      }
+    }
+    
+    */
+
+
+
     // 5페이지까지 가더라도 더보기 버튼으로 다음으로가기
     var nBtn = indiCon.find('.next_notice');
     var pBtn = indiCon.find('.prev_notice');
 
     nBtn.on('click', function(e){
       e.preventDefault();
-      var nbn = memoryN;
+      var nbn = memoryN; // 1) 5로 만들어주기
       indiLi.hide();
+
+
       // nbn이 어디까지 기억하게 할것인지? 5개인 indiViewLen
+      // nbn은 숫자라 수식 밖으로 빼내게되면 카운트가 계속 올라간다
+      // nbn( ) + indiViewLen(5) 이상황에서 값은 전가되지 않는다
+      // nbn만 카운트 올라감
       for( ; nbn < memoryN + indiViewLen ; nbn += 1 ){
         indiLi.eq(nbn).show();
       }//for
-      //모든 처리가 끝났을 때에는 
+      //모든 처리가 끝났을 때에는 (두번째 다음버튼 누르는 
+      
+      // 상황을 동작하게하려고 nbn=memory 라고 해준다
       memoryN = nbn;
     }) //nBtn
 
