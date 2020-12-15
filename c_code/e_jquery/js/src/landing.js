4dfe937929a7a26e9b8306e81e07feda7982ff70
soo330 <SooPark93@gmail.com>
2020-12-15 16:46:14 +0900

    moreBox if/ add add more/ remove action

--- c_code/e_jquery/js/src/landing.js
+++ c_code/e_jquery/js/src/landing.js
@@ -16,20 +16,22 @@
     var getScroll = win.scrollTop()+ winH; //스크롤 값
     // console.log(getScroll);
 
-    // 스크롤 값이, conBox의 offset().top만큼에서 (브라우저 높이값 일부빠진만큼) 보다 크면 수행하라
-    // 더 값이 커졌을 때, moreBox offset값이 화면의 상단에 위치하면
+    
     if(getScroll >= conBoxOffset-winH ){
       conBox.addClass('action');
     }else{
       conBox.removeClass('action');
     }
 
-
-    if ( getScroll >= moreBoxOffset-winH){
-      moreBox.addClass('action');
-    }else if( getScroll >= (moreBoxOffset + winH) +100 ){
-      // moreBox.removeClass('action');
+    // 스크롤 값이, conBox의 offset().top만큼에서 (브라우저 높이값 일부빠진만큼) 보다 크면 수행하라
+    // 더 값이 커졌을 때, moreBox offset값이 화면의 상단에 위치하면 수행
+    // 조건문에서는 앞의 기능이 참이면 뒤의 값은 전혀 수행하지 않는다
+    if ( getScroll >= moreBoxOffset){
       moreBox.addClass('more');
+      moreBox.removeClass('action');
+    }else if( getScroll >= (moreBoxOffset - winH) +100 ){
+      // moreBox.removeClass('action');
+      moreBox.addClass('action');
     }
     else{
       moreBox.removeClass('action');
