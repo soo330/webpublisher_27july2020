13c00e3226788ff9deac6a5e1f49d6f5deacf036
soo330 <SooPark93@gmail.com>
2020-12-15 16:41:28 +0900

    moreBox offset() if

--- c_code/e_jquery/js/src/landing.js
+++ c_code/e_jquery/js/src/landing.js
@@ -6,27 +6,28 @@
   var conBox = $('#conBox');
   var moreBox = $('#moreBox');
 
-  var winH = win.outerHeight();
+  var winH = win.outerHeight(); 
 
   var conBoxOffset = conBox.offset().top;
-  var moreBoxOffset = moreBox.offset().top;
+  var moreBoxOffset = moreBox.offset().top; 
   console.log(conBoxOffset, moreBoxOffset);
 
   win.on('scroll', function(e){
-    var getScroll = win.scrollTop()+ winH; //스크롤탑보다 많다
+    var getScroll = win.scrollTop()+ winH; //스크롤 값
     // console.log(getScroll);
 
-    if(getScroll >= conBoxOffset ){
+    // 스크롤 값이, conBox의 offset().top만큼에서 (브라우저 높이값 일부빠진만큼) 보다 크면 수행하라
+    if(getScroll >= conBoxOffset-winH ){
       conBox.addClass('action');
     }else{
       conBox.removeClass('action');
     }
 
 
-    if ( getScroll >= moreBoxOffset){
+    if ( getScroll >= moreBoxOffset-winH){
       moreBox.addClass('action');
-    }else if( getScroll >= moreBoxOffset + winH ){
-      moreBox.removeClass('action');
+    }else if( getScroll >= (moreBoxOffset + winH) +100 ){
+      // moreBox.removeClass('action');
       moreBox.addClass('more');
     }
     else{
