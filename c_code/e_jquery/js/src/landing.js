9cb712a2c924cbdeebff8705c298ce260ca57087
soo330 <SooPark93@gmail.com>
2020-12-15 17:48:44 +0900

    1215 landing

--- c_code/e_jquery/js/src/landing.js
+++ c_code/e_jquery/js/src/landing.js
@@ -9,16 +9,22 @@
 
   var conBoxOffset = conBox.offset().top;
   var moreBoxOffset = moreBox.offset().top;
-  console.log( conBoxOffset, moreBoxOffset );
+  // console.log( conBoxOffset, moreBoxOffset );
 
   win.on('scroll', function(e){
     var getScroll = win.scrollTop(); // 스크롤 값
     // console.log( getScroll );
 
+
+    var conScroll;
     // 스크롤값이, 
     // conBox의 offset().top (에서 브라우저 높이값 일부빠진만큼)보다 크면 수행
     if(getScroll >= conBoxOffset - winH){
       conBox.addClass('action');
+      conScroll = getScroll - (conBoxOffset - winH); // 스크롤 - (offset)
+      console.log('conScroll:')
+
+      conBox.css({'backgroundPositionY': (-conScroll/3) +'px'});
     }else{
       conBox.removeClass('action');
     }
