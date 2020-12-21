9667ea4d94c1df77ffa1b0c45f3e2d7c4cde9fde
soo330 <SooPark93@gmail.com>
2020-12-21 17:19:19 +0900

    1221 scrollTop

--- c_code/e_jquery/js/src/parallax.js
+++ c_code/e_jquery/js/src/parallax.js
@@ -16,15 +16,15 @@
     var winSt = win.scrollTop();
     // console.log(winSt); //scroll 위치파악
 
-    viewDiv.eq(0).css({backgroundPositionY: winSt + 'px'})
-    viewDiv.eq(1).css({backgroundPositionY: (winSt/2) + 'px'})
-    viewDiv.eq(2).css({backgroundPositionY: (winSt/3) + 'px'})
-    viewDiv.eq(3).css({backgroundPositionY: (winSt/4) + 'px'})
-    viewDiv.eq(4).css({backgroundPositionY: (winSt/5) + 'px'})
-    viewDiv.eq(5).css({backgroundPositionY: (winSt/6) + 'px'})
-    viewDiv.eq(6).css({backgroundPositionY: (winSt/7) + 'px'})
-    viewDiv.eq(7).css({backgroundPositionY: (winSt/8) + 'px'})
-    viewDiv.eq(8).css({backgroundPositionY: (winSt/10) + 'px'})
+    viewDiv.eq(0).css({backgroundPositionY: winSt + 'px'});
+    viewDiv.eq(1).css({backgroundPositionY: (winSt/2) + 'px'});
+    viewDiv.eq(2).css({backgroundPositionY: (winSt/3) + 'px'});
+    viewDiv.eq(3).css({backgroundPositionY: (winSt/4) + 'px'});
+    viewDiv.eq(4).css({backgroundPositionY: (winSt/5) + 'px'});
+    viewDiv.eq(5).css({backgroundPositionY: (winSt/6) + 'px'});
+    viewDiv.eq(6).css({backgroundPositionY: (winSt/7) + 'px'});
+    viewDiv.eq(7).css({backgroundPositionY: (winSt/8) + 'px'});
+    viewDiv.eq(8).css({backgroundPositionY: (winSt/10) + 'px'});
 
   }) //win.scroll
 
