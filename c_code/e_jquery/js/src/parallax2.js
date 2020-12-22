e0959c9f2abc23265dd02f33294da22efdbf5d2a
soo330 <SooPark93@gmail.com>
2020-12-22 12:38:22 +0900

    1222 parallax airplane

---
+++ c_code/e_jquery/js/src/parallax2.js
@@ -0,0 +1,47 @@
+// parallax2.js
+
+(function($){
+  // parallax2 start
+    var win = $(window);
+    var winH = win.outerHeight();
+  
+    var headBox = $('#headBox');
+    var frontImg = headBox.find('.front_image');
+    var backImg = headBox.find('.back_image');
+  
+    var setN = 1.5;
+  
+    // 브라우저 스크롤시 수행
+    // 1. frontImg가 점점 커지게( transform:scale() );
+    // 2. 점점 사라지게
+    win.on('scroll', function(){
+      var winSt = $(this).scrollTop();
+      var per = winSt / winH;
+      var scaleR = 1+per;
+      var opacityR;
+    
+    if(scaleR <= setN){
+      console.log('scale: ', 1 + per);
+      frontImg.css({transform:'scale(' + scaleR + ')'});
+    }else if(scaleR >= setN + 1){      
+      console.log('opacity: ', opacityR + 1 );
+      opacityR = setN + 1 - per;
+      frontImg.css({ opacity : opacityR });
+    }
+
+    // 비행기 나타나면서 올라가기
+    if( scaleR > setN){
+      // saaleR이 setN보다 커지면
+      var airPer = (scaleR - setN) * 100;
+      console.log (airPer);
+      backImg.css({backgroundPositionY: airPer + '%'});
+    }
+  
+    }); // win.on('scroll')
+  
+  
+
+
+
+  //jQuery end
+})(jQuery);
