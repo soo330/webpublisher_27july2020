9a936cd694c5229b7edc5349c1286daf6c126443
soo330 <SooPark93@gmail.com>
2020-12-23 14:11:09 +0900

    1223 update

--- c_code/e_jquery/js/src/like_pinterest.js
+++ c_code/e_jquery/js/src/like_pinterest.js
@@ -17,19 +17,21 @@
   var cardUl = cardBox.children('ul');
   var liText = '<li><a><div class="img_temp"><img /></div><span></span></a></li>';
 
-  cardUl.append(liText);
-  var cardLi = cardUl.children('li');
-
+  var url = '../img/pinterest/'
+  // cardUl.append(liText);
+ 
   var s = 0;
                   // s++ / +=1 / ++1 모두 같음
   // li 이하 태그들 for문으로 만들기
   for( ; s < 20 ; s++ ){
     cardUl.append(liText);
   } //for 20개의 li tags
-
+  var cardLi = cardUl.children('li');
+  
+  
    // ★★★
    // 책장과도 같다고 생각해보자 나는 책이 많은 상태고 그에맞는 책장을 사려고함
-   // ul에 li갯수만큼 담을수있도록 설정하려면
+   // >>> ul에 li갯수만큼 담을수있도록 설정하려면
    // 1. (li의 가로크기a + li의 마진값(공백) b)
    // 2. 브라우저의 (기준)크기값c
 
@@ -39,27 +41,73 @@
     outerWidth() 는 margin을 제외한 가로값 (padding+border+width)값
   */
 
+  //=====================================================================
+  /* =========== 여기서 부터 브라우저 크기가 변경되면 다시수행 - 함수처리 */
+
+  var BrowserSet = function(){
+  // BrowserSet
+  cardLi.removeAttr('style'); // 함수가 재수행하면 li에 style속성을 제거해야함
+
+  
+  // --------------------------
   var cardLiWidth = cardLi.eq(0).outerWidth();
   var cardMargin = cardLi.eq(0).outerWidth(true)-cardLiWidth; // 이 수식으로 b를 뺴주기 위해 설정
 
   // >> 브라우저 크기를 기준으로 내가 만든 li는 총 몇개를 담을 수 있는가?
-  // winW 나누기 margin값
   var winW = win.outerWidth();
   var liLen = parseInt( winW / ( cardLiWidth + cardMargin ) );
+  // liLen =  정수로 가진 값(윈도우 가로값 나누기 (li가로값+li마진값))
+  // li갯수값
+
   //정수화 처리 : parseInt()
   // 버림, 올림, 반올림: Math.floor(), Math.ceil(), Math.round();
   
   // console.log(liLen);
 
-  // 브라우저 가로값 * 카드li의 가로값 - 카드마진값인b 를 빼주는 공식
-  // cardUl.css({ width: (winW * cardLiWidth) - cardMargin });
-  // var ulWidth =( (cardLiWidth + cardMargin) * liLen ) - cardMargin
-  var ulWidth =( (cardLiWidth + cardMargin) * liLen ) // - cardMargin;
-// 
-;
+  // (브라우저 가로값+ 카드li의 가로값)* li갯수 - 카드마진값인b 를 빼주는 공식
+  // ↓ cardUl.css({ width: (winW * cardLiWidth) - cardMargin });
+  // ↓ var ulWidth =( (cardLiWidth + cardMargin) * liLen ) // - cardMargin
+
+  // 마지막 번째 li의 margin b값을 뺴주어야 함
+  var ulWidth =( ( cardLiWidth + cardMargin) * liLen )-cardMargin;
   cardUl.css({width: ulWidth + 'px'});
-  console.log(ulWidth);
-  console.log(cardUl);
+  // console.log(ulWidth,liLen);
+
+  // >>> li 설정
+  // 1. li순번에서 가로 행에 들어가는 마지막 번째에는 마진 제거
+  // 2. 각각의 행에 들어가는 모든 마지막 위치의 갯수번째 margin 제거
+
+  // js는 0부터 시작하므로 1씩 빼주어야함
+  // cardLi.eq( liLen - 1 ).css({margin:0});
+
+
+  // for( ; lin < ? ; lin +=1 ){} // for
+  var lin = 0;
+  // var liNth = cardLi.length / liLen;
+  var liNth = Math.ceil( cardLi.length / liLen);
+  var n; 
+
+  for( ; lin < liNth ; lin += 1 ){
+    // cardLi.eq(lin * liLen - 1)
+    n = liLen * lin -1;
+    console.log(n);
+    cardLi.eq(n).css({marginRight:0});
+
+  } // for
+
+
+  // 가로에 4개씩에서 4번째 마다 => eq(3*n) 마다 마진값
+  // 4번째 마다라는 것은 4, 8 , 12 , 16 순으로 js에서는 3, 7 , 11, 15 값으로나와야함으로
+  // eq(4*n -1)로 써주어야함
+
+  //★★★ 브라우저 크기가 변경될 때 마다 재측정을 해주어야함
+
+} // BrowserSet()
+BrowserSet();
+
+  win.on('resize', function(){
+    BrowserSet();
+  })// resize
 
   //jQuery end
 })(jQuery);
---
+++ c_code/wwfkorea/js/src/main_missionBox/main_missionBox_img.js
@@ -0,0 +1,10 @@
+// main_missionBox_img.js
+
+(function($){
+  // main_missionBox_img start
+
+  var win = $(window);
+
+  
+  //jQuery end
+})(jQuery);
