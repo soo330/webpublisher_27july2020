07a34d5193800e9be51c825e0b55dde2c184f22e
soo330 <SooPark93@gmail.com>
2020-12-28 23:08:18 +0900

    1228 need to fix url

--- c_code/wwfkorea/js/src/main_newsBox/main_newsBox_img.js
+++ c_code/wwfkorea/js/src/main_newsBox/main_newsBox_img.js
@@ -5,20 +5,50 @@
 
   var newsData = [];
   var i=0;
-  var url = '../img/news_img/';
+  var url = '../img/wwfimg/news_img/';
   var imgT = 'news_';
-  var loremT = 'Lorem';
-  var loremC = 'Lorem ipsum dolor sit amet consectetur adipsicing elit. Ipsum, corporis!';
+  var divClassName = 'news_img_';
+  
 
-  for( ; i < 30 ; k++ ){
+  for( ; i < 30 ; i++ ){
     n = (i+1);
     newsData[i] = {};
     newsData[i].title = 'title' + n;
-    newsData[i].img = url + imgT + '.jpg';
-    
+    newsData[i].imgT = url + imgT + n + '.jpg';
+    newsData[i].divClassName = divClassName;
+    // n을 더해줌으로 n=1번째 순서로 만들어준다
   }//for
+  // console.log(newsData);
 
-  console.log(newsData);
+  var win = $(window);
+  // var newsBox = $('#newsBox');
+  var newsArea = $('.news_area');
+  newsArea.append('<ul></ul>');
+
+  var newsUl = newsArea.children('ul');
+  var liText = '<li><a href="#"><div></div><dl><dt></dt><dd><span></span></dd></dl></a></li>';
+
+  // var divClassName = 'news_img_';
+  var loremT = 'Lorem';
+  var loremC = 'Lorem ipsum dolor sit amet consectetur adipsicing elit. Ipsum, corporis!';
+
+  var s = 0;
+  var listEl;
+  
+  // li이하 태그들 for문으로 형성
+  for( ; s < 30 ; s++ ){
+    newsUl.append(liText);
+    listEl = newsUl.children('li').eq(s);
+    var namedDiv = listEl.find('div').addClass(divClassName +(s+1));
+    namedDiv.css({backgroundImage: 'url('+ url+ divClassName+[n]+')'});
+
+
+    // console.log(namedDiv[s])
+
+    listEl.find('dt').text(loremT);
+    listEl.find('dd').text(loremC);
+  } // for s<30
+  
 
   //jQuery end
 })(jQuery);
