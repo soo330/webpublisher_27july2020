2393855584937b1d7dcb7c5da7be49ed745f5c83
soo330 <SooPark93@gmail.com>
2020-12-28 16:51:52 +0900

    1228 update height

--- c_code/e_jquery/js/src/like_pinterest2.js
+++ c_code/e_jquery/js/src/like_pinterest2.js
@@ -5,14 +5,17 @@
   // 2. 스크롤의 진행 위치값 표시
   // 3. 스크롤 시 일정 위치가 넘어가면 추가 카드가 생성되게 처리
   // 4. Math.random(); 을 이용하여 색상, 높이값 변경
+  // 5. 임시로 셋팅했던 .card의 float:left 삭제
+  // 6. 화면상의 하나의 행에 들어갈 위치값 (left값 설정)
+  // 7. 화면상의 열에 해당하는 위치값(top)값 설정 (position: relative로 수정)
 
   // ===================================================
   // ===================================================
 
-  // 4. math.random
+  // 4-1. math.random color설정
   // ramdom = Math.floor(Math.random() * 2);
 
-  /*
+  /* // 4-1-1.
   var r1 = Math.floor(Math.random() * 16).toString(16);
   var r2 = Math.floor(Math.random() * 16).toString(16);
   var r3 = Math.floor(Math.random() * 16).toString(16);
@@ -20,8 +23,18 @@
   var Rn = function(){
   return  Math.floor(Math.random()*16).toString(16);
   };
+
   // var randomColor = '#'+Rn()+Rn()+Rn();  
   // console.log(randomColor);
+
+  //------------------------------------------
+  //4-2. 높이값 (랜덤수치) 변경
+
+  var RandomHeight = function(){
+    var h = parseInt(Math.random() * 7) * 40; // 7가지 * 40px씩 은 210
+    return h;
+  }// RandomHeight
+  // console.log(h);
 
   //----------------------------------------------
   var headBox = $('#headBox');
@@ -51,21 +64,37 @@
 
   var cardCount = 0;
   var randomColor = [];
+  var cardRandomHeight = [];
   
+  // 4-2. var r1,r2,r3; //for문 안에서 정의해주기
 
   // 3-1 카드값 만들기
   var AppendCardFn = function(){
     cardCount += 50;
 
     for(; i < cardCount; i++){
-      cardArea.append(cardCode);
-      card = cardArea.children('.card').eq(i);
+
+      // r1 = parseInt(Math.random()*16).toString(16);
+      // r2 = parseInt(Math.random()*16).toString(16);
+      // r3 = parseInt(Math.random()*16).toString(16);
+
+      cardArea.append(cardCode); // 카드생성
+      card = cardArea.children('.card').eq(i); // 하나의 높이값을 가진상태
+
       card.find('dt').text( 'title_'+ (i+1) );
-      randomColor = '#'+Rn()+Rn()+Rn(); // 함수 밖에서 배열처리 후 만들어줌
-      card.find('.card_img').css({backgroundColor: randomColor[i]}); 
+      randomColor[i] = '#'+Rn()+Rn()+Rn(); // 함수 밖에서 배열처리 후 만들어줌
+      // randomColor[i] = '#' +r1+r2+r3;
+      card.find('.card_img').css({backgroundColor: randomColor[i]});
+
+      // 4-2. card높이값을 기존값과 random 함수값을 합쳐서 처리
+      cardRandomHeight[i] = card.outerHeight() + RandomHeight();
+      card.css({height: cardRandomHeight[i] + 'px'});
 
     } //for
-    $('.card').css({float:'left'});
+
+    // 4-2 card높이값을 처리해준 이후 아래위 공백을 바꿔줘야함으로 float에서 position으로 바꿔줘야함
+    // $('.card').css({float:'left'});
+
 
   } // AppendCardFn
   AppendCardFn(); // 50개씩 늘어나게됨
