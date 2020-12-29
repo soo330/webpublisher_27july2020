cc1e4193ce69f0e98efa166bf0d96e85b8a2e81b
soo330 <SooPark93@gmail.com>
2020-12-29 12:34:21 +0900

    1229 wwf news_img resize

--- c_code/wwfkorea/js/src/main_newsBox/main_newsBox_img.js
+++ c_code/wwfkorea/js/src/main_newsBox/main_newsBox_img.js
@@ -3,52 +3,118 @@
 (function($){
   // main_newsBox start
 
-  var newsData = [];
-  var i=0;
-  var url = '../img/wwfimg/news_img/';
-  var imgT = 'news_';
-  var divClassName = 'news_img_';
+  $.ajax({
+    url : '../data/main_newsArea_img.json',
+    dataType: 'json',
+    context: document.body
+  }).done(function(data){
+
+    var newsData = data;
+
+    var win = $(window);
+    // var newsBox = $('#newsBox');
+    var newsArea = $('.news_area');
+    newsArea.append('<ul></ul>');
+
+    // -----------------------------
+    var newsUl = newsArea.children('ul');
+    var liText = '<li><a href="#"><div></div><dl><dt></dt><dd><span></span></dd></dl></a></li>';
+    var loremT = 'Lorem';
+    var loremC = 'Lorem ipsum dolor sit amet consectetur adipsicing elit. Ipsum, corporis!';
+
+    var s = 0;
+    var listEl;
+    
+    // li이하 태그들 for문으로 형성
+    // console.log( newsData );
+    for( ; s < 30 ; s++ ){
+      newsUl.append(liText);
+      listEl = newsUl.children('li').eq(s);
+      var namedDiv = listEl.find('div').addClass(newsData[s].divClassName);
+      namedDiv.css({backgroundImage: 'url('+ newsData[s].imgT +')'});
+
+      // console.log(divClassName +(s+1))
+
+      listEl.find('dt').text(loremT);
+      listEl.find('dd').text(loremC);
+    } // for s<30
+    
+
+    //=====================================================================
+    // To-do
+    // 1. 가로 1줄에 들어가는 마지막 번째 li 마진 제거
+
+
+    var newsLi = newsUl.children('li');
+    var newsLiImg = newsLi.children('img');
+
+
+    
+    var BrowserSet = function(){
+
+      // 함수 재 수행시 li style 속성 제거
+      newsLi.removeAttr('style');
+
+      // (li의 가로크기a + li의 마진값(공백) b)===============================
+      var newsLiWidth = newsLi.eq(0).outerWidth();
+      var newsMargin = newsLi.eq(0).outerWidth(true)-newsLiWidth; //b를 빼주기위함
+
+      var winW = win.outerWidth();
+      // 정수 ( browser W / ( liWidth + liMargin )) = li 갯수
+      var liLen = parseInt(winW / (newsLiWidth + newsMargin) );
+
+      // (브라우저 가로값+ 카드li의 가로값)* li갯수 - 카드마진값b
+      //      ↓ newsUl.css({ width: (winW * newsLiWidth) - newsMargin });
+
+      //      ↓ var ulWidth = ((newsLiWidth + newsMargin) * liLen)-newsMargin;
+      var ulWidth = ((newsLiWidth + newsMargin)* liLen)-newsMargin;
+      newsUl.css({width : ulWidth + 'px'});
+      
+      // 갯수값 도출
+      // console.log(ulWidth,liLen);
+      
+
+      
+
+      var x = 0;
+      // 올림(newsLi총갯수 / 가로한줄에 들어갈 li갯수)
+      var liNth = Math.ceil( newsLi.length / liLen );
+      var k;
+      
+      // for문
+      // ex 5개씩에서 5번째마다 -> eq(4*n)마다 마진값
+
+      for( ; x < liNth ; x++ ){
+        // 번째값 계산: (li갯수 * x)-1 
+        // newsLi.eq( x * liLen - 1)
+        k = liLen * x -1; 
+        console.log(k);
+
+        newsLi.eq(k).css({marginRight:0}); // 1. 완료
+
+      } // for x < liNth
+    } //BrowserSet
+    BrowserSet();
+
+    
+    // 브라우저 크기 재 측정시
+    win.on('resize', function(){
+      BrowserSet();
+    }) // resize
   
-
-  for( ; i < 30 ; i++ ){
-    n = (i+1);
-    newsData[i] = {};
-    newsData[i].title = 'title' + n;
-    newsData[i].imgT = url + imgT + n + '.jpg';
-    newsData[i].divClassName = divClassName;
-    // n을 더해줌으로 n=1번째 순서로 만들어준다
-  }//for
-  // console.log(newsData);
-
-  var win = $(window);
-  // var newsBox = $('#newsBox');
-  var newsArea = $('.news_area');
-  newsArea.append('<ul></ul>');
-
-  var newsUl = newsArea.children('ul');
-  var liText = '<li><a href="#"><div></div><dl><dt></dt><dd><span></span></dd></dl></a></li>';
-
-  // var divClassName = 'news_img_';
-  var loremT = 'Lorem';
-  var loremC = 'Lorem ipsum dolor sit amet consectetur adipsicing elit. Ipsum, corporis!';
-
-  var s = 0;
-  var listEl;
-  
-  // li이하 태그들 for문으로 형성
-  for( ; s < 30 ; s++ ){
-    newsUl.append(liText);
-    listEl = newsUl.children('li').eq(s);
-    var namedDiv = listEl.find('div').addClass(divClassName +(s+1));
-    namedDiv.css({backgroundImage: 'url('+ url+ divClassName+[n]+')'});
-
-
-    // console.log(namedDiv[s])
-
-    listEl.find('dt').text(loremT);
-    listEl.find('dd').text(loremC);
-  } // for s<30
-  
-
+  }); // ajax
   //jQuery end
 })(jQuery);
+
+
+
+// (function($){
+//   $.ajax({
+//     url: ...$,
+//     dataType:...,
+//     context:document.body
+//   }).done(function(data){
+//     var data = data;
+
+//   })
+// })(jQuery)
