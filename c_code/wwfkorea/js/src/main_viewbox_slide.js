d99987325302818de95919451d838dab7ef424d7
soo330 <SooPark93@gmail.com>
2021-01-07 16:51:30 +0900

    0107 banner working

--- c_code/wwfkorea/js/src/main_viewbox_slide.js
+++ c_code/wwfkorea/js/src/main_viewbox_slide.js
@@ -3,10 +3,11 @@
   // main_viewBox_slide start
   
   var viewBox = $('#viewBox');
-  var backImg = viewBox.find('.back_img');
+  var backImg = viewBox.find('.back_img'); //ul
   var lastLi = backImg.find('li'); // 인디케이터 맨 마지막 li
   
-  //viewArea 마지막 이미지 복제하기
+  // ==================================
+  // viewArea 마지막 이미지 복제하기
   var backLiLast = lastLi.eq(-1).clone(); //마지막 복제
   backImg.prepend(backLiLast); // 복제요소 0앞부분에 추가하기
   var reLiLen = backImg.find('li'); // 복제 후 갯수 재파악
@@ -14,11 +15,17 @@
 
   backImg.css({'marginLeft': -100+'%', 'width' : reLiLen.length * 100 + '%'}); // backImg 크기변경
   reLiLen.css({'width': (100/ reLiLen.length) +'%'}); //내부 li갯수많큼 크기 변경되게하기
+  // ===================================
 
-
+  // text Part 마지막이미지 복제
   var textPart = viewBox.find('text_part');
-  var textDl = textPart.find('dl');
+  var textWrap = textPart.find('.text_wrap');
+  var textDl = textWrap.find('dl');
   var actionDl = textDl.find('.action');
+
+  // ======================================
+  
+ 
 
   var slideN = 0;
   var timed = 2000;
@@ -36,6 +43,8 @@
     // console.log(theLi);
 
     backImg.stop().animate({'left': slideN * -100 + '%'});
+    textDl.stop().css({'left': slideN * -100 + '%'});
+
 
     indiLi.eq(slideN).siblings().removeClass('action');// 선택된 li의 다른 형제li들에서 action 태그 제거
     indiLi.eq(slideN).addClass('action');
@@ -70,9 +79,8 @@
           // slideN이 마지막li길이-1보다 클때
           slideN = -1;
           backImg.stop().css({'left':slideN * -100 + '%'});
-
+          text
         }//if
-
       }) //backimg
       // textDl.eq(slideN).silbling().removeClass('action');
       // textDl.eq(slideN).addClass('action');
@@ -80,8 +88,7 @@
       indiLi.eq(slideN).addClass('action');
     },timed) //setInterval
   } //Start(){}
-
-  // Start();
+  Start();
 
   //============================
   // viewBox 마우스 올릴 시 일시정지
