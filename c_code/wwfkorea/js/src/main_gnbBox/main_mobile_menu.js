b809039ffbc0c84ae9d98497aec71c78a01e0388
soo330 <SooPark93@gmail.com>
2021-01-18 11:03:05 +0900

    0118 gnb js link in pc html

--- c_code/wwfkorea/js/src/main_gnbBox/main_mobile_menu.js
+++ c_code/wwfkorea/js/src/main_gnbBox/main_mobile_menu.js
@@ -2,14 +2,20 @@
 (function($){
   // main_mobile_menu start
 
-
+  // var win = $(window);
   var headBox = $('#headBox');
   
   var menuIBox = headBox.find('.menuIBox');
   var showMenuBtn = menuIBox.find('.menu_btn');
   var showMenuWrap = headBox.find('.show_menu_wrap');
+  var menuArea =showMenuWrap.find('.menu_area');
+
+  var menuCloseBtn = showMenuWrap.find('.menu_close_btn');
 
 
+  // console.log(winT)
+
+  // 햄버거 메뉴버튼 누르면 메뉴 토글
   showMenuBtn.find('button').on('click', function(e){
     //showMenuBtn을 click하면 기능으로,
     e.preventDefault();
@@ -20,11 +26,15 @@
     }else{
       showMenuWrap.fadeOut();
     }
-
     // console.log(menuDp);
   });
   // console.log(showMenuBtn)
 
+  // 메뉴안에 있는 x닫기 버튼 누르면 메뉴 닫아지기
+  menuCloseBtn.find('button').on('click',function(e){
+    e.preventDefault();
+    showMenuWrap.fadeToggle();
+  });
 
 
   
